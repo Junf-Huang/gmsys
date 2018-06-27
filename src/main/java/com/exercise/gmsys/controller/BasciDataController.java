2276d15aa7158bb731106759be250db605230d2b
Junf-Huang <1205331774@qq.com>
2018-06-27 18:25:45 +0800

    finish pagination

--- src/main/java/com/exercise/gmsys/controller/BasciDataController.java
+++ src/main/java/com/exercise/gmsys/controller/BasciDataController.java
@@ -3,6 +3,8 @@ package com.exercise.gmsys.controller;
 import com.exercise.gmsys.impl.CategoryServiceImp;
 import com.exercise.gmsys.model.Category;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 public class BasciDataController {
 
@@ -22,8 +25,24 @@ public class BasciDataController {
     @GetMapping("/category/findAll")
     public String findAll(@RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum, @RequestParam(defaultValue = "2")Integer pageSize, Model model){
         //pageNum为第几页，pageSize为每页记录条数
-        model.addAttribute("data", categoryServiceImp.findAll(pageNum, pageSize));
+        PageInfo<Category> pageInfo = categoryServiceImp.findAll(pageNum, pageSize);
+
+        log.info("page={}", pageInfo.getPages());
+
+        model.addAttribute("data", pageInfo.getList());
+
+        //获得当前页
+        model.addAttribute("pageNum", pageInfo.getPageNum());
+        //获得一页显示的条数
+        model.addAttribute("pageSize", pageInfo.getSize());
+        //是否是第一页
+        model.addAttribute("isFirstPage", pageInfo.isIsFirstPage());
+        //获得总页数
+        model.addAttribute("totalPages", pageInfo.getPages());
+        //是否是最后一页
+        model.addAttribute("isLastPage", pageInfo.isIsLastPage());
+
         return "files/zcfldetails";
-       // return  categoryServiceImp.findAll(pageNum, pageSize).toString();
+       //return  categoryServiceImp.findAll(pageNum, pageSize).toString();
     }
 }
--- src/main/java/com/exercise/gmsys/impl/CategoryServiceImp.java
+++ src/main/java/com/exercise/gmsys/impl/CategoryServiceImp.java
@@ -3,6 +3,7 @@ package com.exercise.gmsys.impl;
 import com.exercise.gmsys.mapper.CategoryMapper;
 import com.exercise.gmsys.model.Category;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,11 +17,11 @@ public class CategoryServiceImp {
     @Autowired
     CategoryMapper categoryMapper;
 
-    public List<Category> findAll(int pageNum, int pageSize){
+    public PageInfo<Category> findAll(int pageNum, int pageSize){
 
         //对其后的第一个查询分页
         PageHelper.startPage(pageNum, pageSize);
-        return categoryMapper.findAll();
+        return new PageInfo<>(categoryMapper.findAll());
     }
 
 }
